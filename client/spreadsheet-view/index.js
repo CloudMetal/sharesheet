4d83b418b6873d780a085007fef123f39f77f96e
iLan Epstein <ilan.epstein@frogdesign.com>
2013-12-18 13:01:41 -0800

    Rendering spreadsheet table in ui.

--- client/spreadsheet-view/index.js
+++ client/spreadsheet-view/index.js
@@ -14,3 +14,33 @@ function SpreadsheetView(model) {
   this.el = domify(html);
   reactive(this.el, this.model, this);
 };
+
+// Generating html for the thead from the model data
+SpreadsheetView.prototype.thead = function () {
+  var data = this.model.data()[0];
+  var returnString = "";
+
+  for (var i = 0; i < data.length; i++) {
+    returnString += "<th>" + data[i] + "</th>";
+  }
+
+  return returnString;
+};
+
+// Generating html for the tbody from the model data
+SpreadsheetView.prototype.tbody = function () {
+  var data = this.model.data();
+  var returnString = "";
+  var i;
+  var a;
+
+  for (i = 1; i < data.length; i++) {
+    returnString += "<tr>";
+    for (a = 0; a < data[i].length; a++) {
+      returnString += "<td>" + data[i][a] + "</td>";
+    }
+    returnString += "</tr>";
+  }
+
+  return returnString;
+};
