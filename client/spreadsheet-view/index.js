2bec8911dd1d1c2e887685228be48f0d79facfd2
Urban Faubion <urban.faubion@gmail.com>
2013-12-23 14:54:25 -0800

    adding spreadsheet headers and columns. Also refactored save.

--- client/spreadsheet-view/index.js
+++ client/spreadsheet-view/index.js
@@ -7,6 +7,7 @@ var domify = require('domify');
 var html = require('./template');
 var reactive = require('reactive');
 var request = require('superagent');
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
 
 module.exports = SpreadsheetView;
 
@@ -17,76 +18,50 @@ function SpreadsheetView(model) {
 };
 
 // Save the spreadsheet
-SpreadsheetView.prototype.saveSpreadsheet = function (event) {
-  var tableHtml = document.querySelector('table');
-  var putBody = {
-    id: this.model.id(),
-    name: document.querySelector("#spreadsheet-name").innerHTML
-  };
-  var tableData = [[]];
-  var i,
-      a;
-
-  var theadHtml = tableHtml.querySelectorAll('thead>tr th');
-  var tbodyRowsHtml = tableHtml.querySelectorAll('tbody>tr');
-  var tbodyTdHtml;
-  var rowArray;
-
-  // each row gets its own array in data
-  for (i = 0; i < theadHtml.length; i++) {
-    tableData[0][i] = theadHtml[i].innerHTML;
-  }
-
-  // iterate over the rows
-  for (i = 0; i < tbodyRowsHtml.length; i++) {
-    // create a new array for each row.
-    rowArray = [];
-
-    tbodyTdHtml = tbodyRowsHtml[i].querySelectorAll('td');
-    // iterate over the row and add data to data object
-    for (a = 0; a < tbodyTdHtml.length; a++) {
-      rowArray.push(tbodyTdHtml[a].innerHTML);
-    }
-    tableData.push(rowArray)
-  }
-
-  putBody.data = tableData;
-
+SpreadsheetView.prototype.save = function (event) {
   request.put('/api/v1/spreadsheet/' + this.model.id())
     .set('Content-Type', 'application/json')
-    .send(JSON.stringify(putBody))
+    .send(this.model.toJSON())
     .end(function (err, res) {
       if (err) {
         console.log('error updating spreadsheet', err);
       }
       console.log(res, 'res');
     });
-
 };
 
-// Generating html for the thead from the model data
-SpreadsheetView.prototype.thead = function () {
-  var data = this.model.data()[0];
-  var returnString = "";
-
-  for (var i = 0; i < data.length; i++) {
-    returnString += "<th contenteditable=\"true\">" + data[i] + "</th>";
-  }
-
-  return returnString;
+SpreadsheetView.prototype.contentChange = function (e) {
+  console.log('content change');
+  var cell = e.target.dataset.cell.split(':');
+  var row = cell[0];
+  var column = cell[1];
+  var value = e.target.innerText;
+  console.log(cell);
+  console.log(value);
+  this.model.data()[row][column] = value;
 };
 
 // Generating html for the tbody from the model data
-SpreadsheetView.prototype.tbody = function () {
+SpreadsheetView.prototype.table = function () {
   var data = this.model.data();
   var returnString = "";
   var i;
   var a;
 
-  for (i = 1; i < data.length; i++) {
+  var columns = data[0].length;
+  returnString += "<tr>";
+  returnString += "<td></td>"; // spacer
+  for (i = 0; i < columns; i++) {
+    returnString += "<td>" + alphabet[i] + "</td>";
+  }
+  returnString += "</tr>";
+
+  for (i = 0; i < data.length; i++) {
     returnString += "<tr>";
     for (a = 0; a < data[i].length; a++) {
-      returnString += "<td contenteditable=\"true\">" + data[i][a] + "</td>";
+      // add row headers
+      if (a == 0) returnString += "<td>" + (i + 1) + "</td>";
+      returnString += "<td contenteditable=\"true\" data-cell=\"" + i + ':' + a + "\">" + data[i][a] + "</td>";
     }
     returnString += "</tr>";
   }
